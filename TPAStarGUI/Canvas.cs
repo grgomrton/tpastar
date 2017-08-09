e358eb86a7e4f4ebefbf0537ca29723d5d76d548
grgomrton <grgo.mrton@gmail.com>
2017-08-09 22:58:59 +0200

    removing unnecessary classfile DrawMethod

--- TPAStarGUI/Canvas.cs
+++ TPAStarGUI/Canvas.cs
@@ -25,6 +25,18 @@ namespace TPAStarGUI
         private float magnify;
 
         /// <summary>
+        /// A delegate function that can be used to implement drawing functions.
+        /// An instance of a <see cref="DrawMethod"/> can be added to a 
+        /// <see cref="Canvas"/>.One object can implement multiple draw methods, 
+        /// displaying different type of data. Every draw method using the same 
+        /// canvas should also use identical coordinate systems.
+        /// </summary>
+        /// <param name="canvas">The canvas.</param>
+        /// <param name="colors">The colors used for drawing. See the used draw method for details.</param>
+        /// <param name="widths">The widths used for drawing. See the used draw method for details.</param>
+        public delegate void DrawMethod(Graphics canvas, Dictionary<string, Color> colors, Dictionary<string, float> widths);
+        
+        /// <summary>
         /// Initializes a new instance of the <see cref="Canvas"/> class.
         /// </summary>
         public Canvas()
