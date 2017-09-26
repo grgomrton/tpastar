043094c048ed5e35f425062b640d23a8f16a488f
grgomrton <grgo.mrton@gmail.com>
2017-09-26 22:32:57 +0200

    add timeout to pathfinder tests
    test target version changed to 4.5

--- Tests/Pathfinder/TPAStarTests.cs
+++ Tests/Pathfinder/TPAStarTests.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
 using TriangulatedPolygonAStar.BasicGeometry;
@@ -10,6 +15,8 @@ namespace TriangulatedPolygonAStar.Tests
     public class TPAStarTests
     {
         private static double Precision = 0.00001;
+        private static int TimeOutInMillseconds = 1000;
+        // in order to have timeout support, assert and act sections are switched
         
         [Test]
         public void pathBetweenStartAndGoalInTheSameTriangleShouldBeAStraightLine()
@@ -20,18 +27,26 @@ namespace TriangulatedPolygonAStar.Tests
             var t1 = new Triangle(a, b, c);
             var s = new Vector(5.5, 7.5);
             var g = new Vector(7.0, 7.5);
-            var solver = new TPAStarPathFinder();
+            var tpaStar = new TPAStarPathFinder();
+            var cancelAfterTimeout = new CancellationTokenSource(TimeOutInMillseconds).Token;
+
+            Action<Task<IEnumerable<IVector>>> pathAssertion = pathFindingOutcome =>
+            {
+                var path = pathFindingOutcome.Result.ToList();
+                path.Count.Should().Be(2);
+                var pathLength = path.Zip(path.Skip(1), (v1, v2) => v1.DistanceFrom(v2)).Sum();
+                pathLength.Should().BeApproximately(1.5, Precision);
+                var pathNodes = path.ToList();
+                pathNodes[0].X.Should().BeApproximately(5.5, Precision);
+                pathNodes[0].Y.Should().BeApproximately(7.5, Precision);
+                pathNodes[1].X.Should().BeApproximately(7.0, Precision);
+                pathNodes[1].Y.Should().BeApproximately(7.5, Precision);
+            };
             
-            var path = solver.FindPath(s, t1, new [] { g });
-            
-            path.Count.Should().Be(2);
-            var pathLength = path.Zip(path.Skip(1), (v1, v2) => v1.DistanceFrom(v2)).Sum();
-            pathLength.Should().BeApproximately(1.5, Precision);
-            var pathNodes = path.ToList();
-            pathNodes[0].X.Should().BeApproximately(5.5, Precision);
-            pathNodes[0].Y.Should().BeApproximately(7.5, Precision);
-            pathNodes[1].X.Should().BeApproximately(7.0, Precision);
-            pathNodes[1].Y.Should().BeApproximately(7.5, Precision);
+            Task<IEnumerable<IVector>>.Factory
+                .StartNew(() => tpaStar.FindPath(s, t1, new [] { g }), cancelAfterTimeout)
+                .ContinueWith(pathAssertion, cancelAfterTimeout)
+                .Wait(2 * TimeOutInMillseconds);
         }
         
         [Test]
@@ -49,18 +64,26 @@ namespace TriangulatedPolygonAStar.Tests
             t2.SetNeighbours(new [] {t1});
             var s = new Vector(7.5, 7.5);
             var g = new Vector(7.5, 10.0);
-            var solver = new TPAStarPathFinder();
+            var tpaStar = new TPAStarPathFinder();
+            var cancelAfterTimeout = new CancellationTokenSource(TimeOutInMillseconds).Token;
+
+            Action<Task<IEnumerable<IVector>>> pathAssertion = pathFindingOutcome =>
+            {
+                var path = pathFindingOutcome.Result.ToList();
+                path.Count.Should().Be(2);
+                var pathLength = path.Zip(path.Skip(1), (v1, v2) => v1.DistanceFrom(v2)).Sum();
+                pathLength.Should().BeApproximately(2.5, Precision);
+                var resultPath = path.ToList();
+                resultPath[0].X.Should().BeApproximately(7.5, Precision);
+                resultPath[0].Y.Should().BeApproximately(7.5, Precision);
+                resultPath[1].X.Should().BeApproximately(7.5, Precision);
+                resultPath[1].Y.Should().BeApproximately(10.0, Precision);
+            };
             
-            var path = solver.FindPath(s, t1, new IVector[] { g });
-            
-            path.Count.Should().Be(2);
-            var pathLength = path.Zip(path.Skip(1), (v1, v2) => v1.DistanceFrom(v2)).Sum();
-            pathLength.Should().BeApproximately(2.5, Precision);
-            var resultPath = path.ToList();
-            resultPath[0].X.Should().BeApproximately(7.5, Precision);
-            resultPath[0].Y.Should().BeApproximately(7.5, Precision);
-            resultPath[1].X.Should().BeApproximately(7.5, Precision);
-            resultPath[1].Y.Should().BeApproximately(10.0, Precision);
+            Task<IEnumerable<IVector>>.Factory
+                .StartNew(() => tpaStar.FindPath(s, t1, new IVector[] { g }), cancelAfterTimeout)
+                .ContinueWith(pathAssertion, cancelAfterTimeout)
+                .Wait(2 * TimeOutInMillseconds);
         }
 
         [Test]
@@ -91,16 +114,24 @@ namespace TriangulatedPolygonAStar.Tests
             t5.SetNeighbours(new[] {t4});
             var s = new Vector(9.0, 11.5);
             var g = new Vector(12.0, 13.5);
-            var solver = new TPAStarPathFinder();
-            
-            var path = solver.FindPath(s, t2, new IVector[] { g });
-            
-            path.Count.Should().Be(3);
-            var pathLength = path.Zip(path.Skip(1), (v1, v2) => v1.DistanceFrom(v2)).Sum();
-            pathLength.Should().BeApproximately(sqrt2PlusSqrt5, Precision);
-            var pathNodes = path.ToList();
-            pathNodes[1].X.Should().BeApproximately(10.0, Precision);
-            pathNodes[1].Y.Should().BeApproximately(12.5, Precision);
+            var tpaStar = new TPAStarPathFinder();
+            var cancelAfterTimeout = new CancellationTokenSource(TimeOutInMillseconds).Token;
+
+            Action<Task<IEnumerable<IVector>>> pathAssertion = pathFindingOutcome =>
+            {
+                var path = pathFindingOutcome.Result.ToList();
+                path.Count.Should().Be(3);
+                var pathLength = path.Zip(path.Skip(1), (v1, v2) => v1.DistanceFrom(v2)).Sum();
+                pathLength.Should().BeApproximately(sqrt2PlusSqrt5, Precision);
+                var pathNodes = path.ToList();
+                pathNodes[1].X.Should().BeApproximately(10.0, Precision);
+                pathNodes[1].Y.Should().BeApproximately(12.5, Precision);
+            };
+
+            Task<IEnumerable<IVector>>.Factory
+                .StartNew(() => tpaStar.FindPath(s, t2, new IVector[] {g}), cancelAfterTimeout)
+                .ContinueWith(pathAssertion, cancelAfterTimeout)
+                .Wait(2 * TimeOutInMillseconds);
         }
         
         [Test]
@@ -129,18 +160,26 @@ namespace TriangulatedPolygonAStar.Tests
             var s = new Vector(9.0, 11.5);
             var g1 = new Vector(12.0, 13.5);
             var g2 = new Vector(9.0, 14.5);
-            var solver = new TPAStarPathFinder();
+            var tpaStar = new TPAStarPathFinder();
+            var cancelAfterTimeout = new CancellationTokenSource(TimeOutInMillseconds).Token;
+
+            Action<Task<IEnumerable<IVector>>> pathAssertion = pathFindingOutcome =>
+            {
+                var path = pathFindingOutcome.Result.ToList();
+                path.Count.Should().Be(2);
+                var pathLength = path.Zip(path.Skip(1), (v1, v2) => v1.DistanceFrom(v2)).Sum();
+                pathLength.Should().BeApproximately(3.0, Precision);
+                var pathNodes = path.ToList();
+                pathNodes[0].X.Should().BeApproximately(9.0, Precision);
+                pathNodes[0].Y.Should().BeApproximately(11.5, Precision);
+                pathNodes[1].X.Should().BeApproximately(9.0, Precision);
+                pathNodes[1].Y.Should().BeApproximately(14.5, Precision);                
+            };
             
-            var path = solver.FindPath(s, t2, new IVector[] { g1, g2 });
-            
-            path.Count.Should().Be(2);
-            var pathLength = path.Zip(path.Skip(1), (v1, v2) => v1.DistanceFrom(v2)).Sum();
-            pathLength.Should().BeApproximately(3.0, Precision);
-            var pathNodes = path.ToList();
-            pathNodes[0].X.Should().BeApproximately(9.0, Precision);
-            pathNodes[0].Y.Should().BeApproximately(11.5, Precision);
-            pathNodes[1].X.Should().BeApproximately(9.0, Precision);
-            pathNodes[1].Y.Should().BeApproximately(14.5, Precision);
+            Task<IEnumerable<IVector>>.Factory
+                .StartNew(() => tpaStar.FindPath(s, t2, new IVector[] {g1, g2}), cancelAfterTimeout)
+                .ContinueWith(pathAssertion, cancelAfterTimeout)
+                .Wait(2 * TimeOutInMillseconds);
         }
         
         // TODO add test for not entering the same triangle due to exceeding edge threshold
